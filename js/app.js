d06fcd1ca121c3a212a8a198bea18777159e5c60
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-03-01 00:07:46 +0600

    display phone done

--- js/app.js
+++ js/app.js
@@ -9,14 +9,34 @@ const searchPhones = ()=>{
     .then(data =>displaySearchResult(data.data))
 }
 // searchPhones()
+
+/*=========DisplayPhone=============== */
+
 const displaySearchResult = phones =>{
     // console.log(phones)
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    phones.forEach(phone =>{
-        console.log(phone);
+    phones.slice(0,20).forEach(phone =>{
+    //   console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand}</h5>
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.slug}</p>
+
+            </div>
+       
+        </div>
+        `;
+        searchResult.appendChild(div);
+      
+     
     })
   
-
-
 }
+
